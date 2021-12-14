72aec6c0f56acd591184983444294961bc33529c
Akbal-21 <akba153@hotmail.com>
2021-12-14 13:33:09 -0600

    creacion del git

--- src/components/screen/HomeScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const HomeScreen = () => {
-    return (
-        <div>
-            <h1>Hola mundo</h1>
-        </div>
-    )
-}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { AdooApp } from './AdooApp';
-import './styles/styles.scss';
-
-ReactDOM.render(
-    <AdooApp />,
-    document.getElementById('root')
-);
